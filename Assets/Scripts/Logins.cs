d3770eeb6f4a6eadaa1104a2c9a616cba69236fc
NTam <nichole.cy.tam@gmail.com>
2017-07-24 16:22:46 +1000

    Added Login, Create Account

---
+++ Assets/Scripts/Logins.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logins : MonoBehaviour
+{
+    public string username = "username";
+    public string password = "password";
+    public string email = "email";
+    public void CreateAccountButton()
+    {
+        StartCoroutine(CreateUser(username,password,email));
+    }
+    IEnumerator CreateUser(string userName, string passWord, string eMail)
+    {
+        string createUserURL = "localhost/unicorns_dee/InsertUser.php";
+
+        WWWForm userInfo = new WWWForm();
+        userInfo.AddField("usernamePost", userName);
+        userInfo.AddField("passwordPost", userName);
+        userInfo.AddField("emailPost", userName);
+        WWW www = new WWW(createUserURL, userInfo);
+
+        yield return www;
+
+        Debug.Log(www.text);
+    }
+}
