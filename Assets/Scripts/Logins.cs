ec59f670a7dbe523973d11d276115e6382dfce1d
NTam <nichole.cy.tam@gmail.com>
2017-08-05 23:57:50 +1000

    Connected Input Fields and UI Screens

--- Assets/Scripts/Logins.cs
+++ Assets/Scripts/Logins.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Net;
 using System.Net.Security;
@@ -12,6 +13,66 @@ public class Logins : MonoBehaviour
     public string username = "username";
     public string password = "password";
     public string email = "email";
+
+    public InputField usernameInput, passwordInput, emailInput, loginUsernameInput, loginPasswordInput, forgotEmailInput;
+
+    public bool showLogin, showCreateAcc, showForgotPw;
+    public GameObject login, createAcc, forgotPw;
+
+    void Start()
+    {
+        login.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (usernameInput.text != username)
+        {
+            username = usernameInput.text;
+        }
+        if (passwordInput.text != password)
+        {
+            password = passwordInput.text;
+        }
+        if (emailInput.text != email)
+        {
+            email = emailInput.text;
+        }
+        if(loginUsernameInput.text != username)
+        {
+            username = loginUsernameInput.text;
+        }
+        if (loginPasswordInput.text != password)
+        {
+            password = loginPasswordInput.text;
+        }
+        if (forgotEmailInput.text != email)
+        {
+            email = emailInput.text;
+        }
+    }
+
+    public void ShowCreateAcc()
+    {
+        createAcc.SetActive(true);
+        forgotPw.SetActive(false);
+        login.SetActive(false);
+    }
+
+    public void ShowLogin()
+    {
+        login.SetActive(true);
+        forgotPw.SetActive(false);
+        createAcc.SetActive(false);
+    }
+
+    public void ShowForgotPw()
+    {
+        forgotPw.SetActive(true);
+        login.SetActive(false);
+        createAcc.SetActive(false);
+    }
+
     public void CreateAccountButton()
     {
         StartCoroutine(CreateUser(username,password,email));
