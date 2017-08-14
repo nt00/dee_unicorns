0d7a092c9de4929f4b7cdcad884839fbbc9edb7d
NTam <nichole.cy.tam@gmail.com>
2017-08-14 15:56:12 +1000

    Added Inventory/Weapon Database

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public string[] items;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LoadItemData());
+    }
+
+    IEnumerator LoadItemData()
+    {
+        WWW itemsURL = new WWW("localhost/unicorns_dee/ItemData.php");
+        yield return itemsURL;
+        string itemData = itemsURL.text;
+        items = itemData.Split(';');
+    }
+}
